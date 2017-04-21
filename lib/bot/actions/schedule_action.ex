8e195965c7d573d0552b0e94875560190e4f311b
sumimaru <sumiyoshi102@gmail.com>
2017-04-21 16:19:07 +0900

    Update schedule_action.ex

--- lib/bot/actions/schedule_action.ex
+++ lib/bot/actions/schedule_action.ex
@@ -1,15 +1,10 @@
 defmodule Bot.ScheduleAction do
-  @moduledoc false
 
   alias Bot.Registry
   alias Slack.Sends
 
   def hello() do
-    send_message("こんにちは")
-  end
-
-  def send_message(message) do
-    Sends.send_message(message, Registry.get_id(), Registry.get_slack())
+    Sends.send_message("こんにちは", Registry.get_id(), Registry.get_slack())
   end
 
 end
